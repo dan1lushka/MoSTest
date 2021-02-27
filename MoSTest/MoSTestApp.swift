515c1a9e7bb935b64df09272f9cd1d0fdfe10f7e
Daniel Rybak <daniel.rybak91@gmail.com>
2021-02-27 13:26:29 +0000

    Initial Commit

---
+++ MoSTest/MoSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoSTestApp.swift
+//  MoSTest
+//
+//  Created by Daniel Rybak on 27/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
