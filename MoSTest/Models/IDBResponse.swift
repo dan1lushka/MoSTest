a18eeb4e30b1581dd3f89d51fc4c7014003c7bce
Daniel Rybak <daniel.rybak91@gmail.com>
2021-02-27 18:43:35 +0000

    add errorMessage property to IDBResponse

--- MoSTest/Models/IDBResponse.swift
+++ MoSTest/Models/IDBResponse.swift
@@ -10,16 +10,17 @@ import SwiftUI
 
 struct IDBResponse: Codable {
     var items: [Item]
+    var errorMessage: String
 }
 
 struct Item: Identifiable, Codable {
-    var id: String = ""
-    var rank: String = ""
-    var title: String = ""
-    var fullTitle: String = ""
-    var year: String = ""
-    var image: String = ""
-    var crew: String = ""
-    var imDbRating: String = ""
-    var imDbRatingCount: String = ""
+    var id: String
+    var rank: String
+    var title: String
+    var fullTitle: String
+    var year: String
+    var image: String
+    var crew: String
+    var imDbRating: String
+    var imDbRatingCount: String
 }
