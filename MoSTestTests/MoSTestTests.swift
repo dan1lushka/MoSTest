a73556e47a34ecca56abda550731a8fb7def5739
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-06 14:35:58 +0000

    add test class

---
+++ MoSTestTests/MoSTestTests.swift
@@ -0,0 +1,29 @@
+//
+//  MoSTestTests.swift
+//  MoSTestTests
+//
+//  Created by Daniel Rybak on 04/03/2021.
+//
+
+import XCTest
+
+class MoSTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func TestMovieListManagerExample() throws {
+        
+    }
+
+}
